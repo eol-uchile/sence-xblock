03b8543b00089c9a57cc8c1022a87843599eda26
matsalinas <matsalinas@uchile.cl>
2020-07-28 20:21:50 +0000

    Student Course code

--- sence/admin.py
+++ sence/admin.py
@@ -3,16 +3,19 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import EolSenceCourseSetup, EolSenceStudentStatus
+from .models import EolSenceCourseSetup, EolSenceStudentSetup, EolSenceStudentStatus
 
 
 class EolSenceCourseSetupAdmin(admin.ModelAdmin):
-    list_display = ('course', 'sence_code', 'sence_course_code', 'sence_line')
+    list_display = ('course', 'sence_code', 'sence_line')
 
+class EolSenceStudentSetupAdmin(admin.ModelAdmin):
+    list_display = ('user', 'course', 'sence_course_code')
 
 class EolSenceStudentStatusAdmin(admin.ModelAdmin):
     list_display = ('user', 'course', 'created_at', 'expires_at')
 
 
 admin.site.register(EolSenceCourseSetup, EolSenceCourseSetupAdmin)
+admin.site.register(EolSenceStudentSetup, EolSenceStudentSetupAdmin)
 admin.site.register(EolSenceStudentStatus, EolSenceStudentStatusAdmin)
