5371a282b1f189c3334920493928df38f28e50fa
matsalinas <matsalinas@uchile.cl>
2021-03-08 15:59:13 +0000

    Update Koa

--- .github/test.sh
+++ .github/test.sh
@@ -3,14 +3,14 @@
 pip install -e git+https://github.com/eol-uchile/uchileedxlogin@3a59e9a4b898786549893ee0c651796ba97199dc#egg=uchileedxlogin
 pip install -e /openedx/requirements/sence-xblock
 
-cd /openedx/requirements/sence-xblock/sence
+cd /openedx/requirements/sence-xblock
 cp /openedx/edx-platform/setup.cfg .
 mkdir test_root
 cd test_root/
 ln -s /openedx/staticfiles .
 
-cd /openedx/requirements/sence-xblock/sence
+cd /openedx/requirements/sence-xblock
 
-DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest tests.py
+DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest sence/tests.py
 
 rm -rf test_root
