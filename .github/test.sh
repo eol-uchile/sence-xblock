5a3cb8a7e6a00736d24df7e7af010e4e024f2b1c
Thomas Peet <tomaspemora@gmail.com>
2025-05-20 19:57:06 -0400

    fix: update test script dependencies

--- .github/test.sh
+++ .github/test.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-pip install --src /openedx/venv/src -e git+https://github.com/eol-uchile/uchileedxlogin@0.0.1#egg=uchileedxlogin
+# pip install --src /openedx/venv/src -e git+https://github.com/eol-uchile/uchileedxlogin@0.0.1#egg=uchileedxlogin
+pip install --src /openedx/venv/src -e git+https://github.com/eol-uchile/eol_sso_login@0.1.1#egg=eol_sso_login
 pip install --src /openedx/venv/src -e /openedx/requirements/app
 pip install pytest-cov genbadge[coverage]
 
@@ -14,10 +15,5 @@ ln -sfn /openedx/staticfiles ./test_root/
 DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest sence/tests.py
 
 rm -rf test_root
-
-pip uninstall -e git+https://github.com/eol-uchile/uchileedxlogin@0.0.1#egg=uchileedxlogin
-pip install --src /openedx/venv/src -e git+https://github.com/eol-uchile/eol_sso_login@0.1.1#egg=eol_sso_login
-DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest sence/tests.py
-
 
 genbadge coverage
