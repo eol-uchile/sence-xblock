eb5ed738ac8244c117e161012346de3b01a18654
Thomas Peet <tomaspemora@gmail.com>
2025-05-20 15:08:51 -0400

    fix: update test script to test both login modules

--- .github/test.sh
+++ .github/test.sh
@@ -8,11 +8,16 @@ pip install pytest-cov genbadge[coverage]
 
 cd /openedx/requirements/app
 
-mkdir test_root
-ln -s /openedx/staticfiles ./test_root/
+mkdir -p test_root
+ln -sfn /openedx/staticfiles ./test_root/
 
 DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest sence/tests.py
 
 rm -rf test_root
+
+pip uninstall -e git+https://github.com/eol-uchile/uchileedxlogin@0.0.1#egg=uchileedxlogin
+pip install --src /openedx/venv/src -e git+https://github.com/eol-uchile/eol_sso_login@0.1.1#egg=eol_sso_login
+DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest sence/tests.py
+
 
 genbadge coverage
